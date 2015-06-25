48b1b3a581d8b18a9ea939becd57a81e945fd482
Christophe Larsonneur <clarsonneur@gmail.com>
2015-06-25 14:33:48 +0200

    Lorj 1.0.15 requires :lib_name during process declaration
    Change-Id: I23559b98f4f699329985859bc15d773f463eb99c

--- lib/lorj_cloud.rb
+++ lib/lorj_cloud.rb
@@ -3,4 +3,4 @@ require 'lorj_cloud/version'
 require 'lorj'
 
 Lorj.declare_process('cloud', File.dirname(__FILE__),
-                     :controllers_dir => 'providers')
+                     :controllers_dir => 'providers', :lib_name => 'lorj_cloud')
